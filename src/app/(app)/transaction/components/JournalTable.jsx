cf9a6e657411ae0e399d7c87129e8c4e27529283
fendnugraha <fend@fendnugrahas-MacBook-Air.local>
2025-06-03 19:08:14 +0700

    add error network message

--- src/app/(app)/transaction/components/JournalTable.jsx
+++ src/app/(app)/transaction/components/JournalTable.jsx
@@ -228,21 +228,22 @@ const JournalTable = ({
                     </button>
                 </Modal>
             </div>
-            <div className="px-4 pt-2 flex">
-                <button className="rounded-l-lg bg-white py-2 px-6 border  border-gray-300">
-                    <SearchIcon size={20} />
-                </button>
-                <Input
+            <div className="relative w-full sm:max-w-sm ml-4 my-2">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <SearchIcon className="w-6 h-6 text-gray-500" />
+                </div>
+                <input
                     type="search"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     placeholder="Search..."
-                    className="w-full rounded-e-lg rounded-l-none border p-2 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    className="block w-full text-sm mb-2 pl-10 pr-4 py-2 text-gray-900 placeholder-gray-400 bg-white border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 />
             </div>
             <div className="px-4 pt-1">
                 <h4 className="text-xs text-slate-500">
-                    {warehouses?.data?.find((w) => w.id === Number(selectedWarehouse))?.name}, {formatLongDate(startDate)} s/d {formatLongDate(endDate)}
+                    {warehouses?.data?.find((w) => w.id === Number(selectedWarehouse))?.name},{" "}
+                    {startDate === endDate ? formatLongDate(endDate) : `${formatLongDate(startDate)} s/d ${formatLongDate(endDate)}`}
                 </h4>
             </div>
             <div className="overflow-x-auto">
--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "@/libs/auth";
 import Image from "next/image";
 import Loading from "./loading";
+import { SatelliteDishIcon } from "lucide-react";
 
 const LoginPage = () => {
     const [email, setEmail] = useState("");
@@ -20,7 +21,8 @@ const LoginPage = () => {
 
     useEffect(() => {
         if (authError) {
-            setStatus(authError.message);
+            const status = authError?.code === "ERR_NETWORK" ? "Network Error, trying to reach the server" : authError?.message;
+            setStatus(status);
         }
     }, [authError]);
 
@@ -42,39 +44,53 @@ const LoginPage = () => {
             <div className="bg-white p-6 rounded-2xl shadow-2xl w-full sm:w-[400px] md:1/3">
                 <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Login to Your Account</h2>
                 <form onSubmit={handleSubmit} className="space-y-4">
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
-                            Email address
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full p-2 border border-gray-300 rounded-md"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full p-2 border border-gray-300 rounded-md"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        {errors.password && <p className="text-red-500 text-xs">{errors.password}</p>}
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={loading || message === "Login successful!"}
-                        className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
-                    >
-                        {loading || message === "Login successful!" ? "Loging in ..." : "Login"}
-                    </button>
+                    {authError?.code !== "ERR_NETWORK" ? (
+                        <>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="w-full p-2 border border-gray-300 rounded-md"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="w-full p-2 border border-gray-300 rounded-md"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                {errors.password && <p className="text-red-500 text-xs">{errors.password}</p>}
+                            </div>
+
+                            <button
+                                type="submit"
+                                disabled={loading || message === "Login successful!" || authError?.code === "ERR_NETWORK"}
+                                className={`w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed `}
+                            >
+                                {loading || message === "Login successful!"
+                                    ? "Loging in ..."
+                                    : authError?.code === "ERR_NETWORK"
+                                    ? "Retrying connection.."
+                                    : "Login"}
+                            </button>
+                        </>
+                    ) : (
+                        <div className="mb-4 flex flex-col gap-2 justify-center items-center animate-pulse">
+                            <SatelliteDishIcon size={150} strokeWidth={2} className=" text-red-500 mx-auto" />
+                            <span className="text-slate-500 text-xs">Retrying connection, please wait..</span>
+                        </div>
+                    )}
                 </form>
                 <p className="text-center mt-6 text-xs">
                     &copy; 2022 Jour Apps by <Image src="/eightnite.png" alt="Logo" width={60} height={60} className="inline-block mx-1 w-auto" /> All rights
