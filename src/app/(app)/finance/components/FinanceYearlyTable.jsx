69ac5b1de1a1f43b243616d738e7960f6899794f
fendnugraha <fend@fendnugrahas-MacBook-Air.local>
2025-07-08 18:19:27 +0700

    add yearly finance add loading, ganti format bulan

--- src/app/(app)/finance/components/FinanceYearlyTable.jsx
+++ src/app/(app)/finance/components/FinanceYearlyTable.jsx
@@ -23,7 +23,7 @@ const FinanceYearlyTable = ({ financeType }) => {
     useEffect(() => {
         fetchFinanceYearly();
     }, [year]);
-    const monthNames = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthNames = ["", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 
     const filteredFinanceByType = finance?.finance?.filter((item) => item.finance_type === financeType);
     console.log(financeType);
